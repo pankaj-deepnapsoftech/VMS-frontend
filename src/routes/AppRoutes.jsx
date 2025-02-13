c45f74c6fe147fa257a79f51596a2ae6010454c2
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-02-13 18:14:38 +0530

    some changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -38,7 +38,7 @@ const AppRoutes = () => {
       {/* Protected routes */}
       {isAuthenticated  ? (
         <Route element={<MainLayout />}>
-          {(authenticate.role === "Admin"  ? PrivateRoutes :authenticate.role === "Assessor" ? EmployeeRoutes : authenticate.role === "ClientSME"  ? ClientSmeRoutes : []).map((item, index) => (
+          {(authenticate.role === "Admin"  ? PrivateRoutes :authenticate.role === "Assessor" && authenticate.employee_approve ? EmployeeRoutes : authenticate.role === "ClientSME"  ? ClientSmeRoutes : [{path:"/",element:<PageNotFound />}]).map((item, index) => (
             <Route key={index} path={item.path} element={item.element} />
           ))}
         </Route>
