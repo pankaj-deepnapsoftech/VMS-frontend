20d1a3f666ee75d908ae0b05985d804734c3f798
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-05-13 17:33:16 +0530

    some changes

--- src/routes/layouts/MainLayout.jsx
+++ src/routes/layouts/MainLayout.jsx
@@ -28,12 +28,12 @@ const MainLayout = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [showUserMenu, setShowUserMenu] = useState(false);
 
-  const [getDataFromSession,setGetDataFromSession] = useState(() => {
-  return  sessionStorage.getItem("main-page");
-})
+  const [getDataFromSession, setGetDataFromSession] = useState(() => {
+    return sessionStorage.getItem("main-page");
+  })
 
- 
- console.log("this is something ",getDataFromSession)
+
+  console.log("this is something ", getDataFromSession)
 
   let notificationcount =
     notificationData?.filter((notification) => !notification.view).length || 0;
@@ -68,63 +68,63 @@ const MainLayout = () => {
 
   return (
     !getDataFromSession ? <FirstDashboard setGetDataFromSession={setGetDataFromSession} /> :
-   ( 
-   <Suspense fallback={<Loader/>}>
+      (
+        <Suspense fallback={<Loader />}>
 
-   
-      <aside
-        className={`${showMenu ? "left-0" : "-left-full"
-          } fixed z-10 w-[65%] flex flex-col justify-between 
+
+          <aside
+            className={`${showMenu ? "left-0" : "-left-full"
+              } fixed z-10 w-[65%] flex flex-col justify-between 
 h-screen  bg-gradient-to-t from-[#151515] to-[#212224] 
 transition duration-300 sm:w-[40%] md:w-[30%] lg:w-[25%] xl:w-[20%] 2xl:w-[15%] `}
-      >
-        <Header
-          setShowMenu={() =>
-            width > 1023 ? setShowMenu(true) : setShowMenu(!showMenu)
-          }
-        />
-      </aside>
-      <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] h-screen bg-background">
-        <div className="bg-gradient-to-t from-[#1a1c1e] to-[#212325]  border-gray-200">
-          <div className="flex items-center justify-between px-2 sm:px-4 py-2">
-            <div className="w-full flex items-center justify-between pr-5">
-              <button
-                className="p-2 hover:bg-gray-100 rounded lg:hidden "
-                onClick={() => setShowMenu(!showMenu)}
-              >
-                <AiOutlineMenu className="w-5 h-5" />
-              </button>
-
-              <h1 className="text-base sm:text-xl text-white font-semibold truncate capitalize">
-                {location.pathname !== "/"
-                  ? temp?.replace("-", " ")
-                  : "Dashboard"}
-              </h1>
-              <div className=" flex items-end justify-end">
-                <button
-                  onClick={() => setSidebarOpen(true)}
-                  className="relative flex items-center gap-2 bg-gradient-to-bl from-[#333333] to-[#666666] text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-                >
-                  <FaBell className="w-6 h-6" />
-                  {notificationcount > 0 ? (
-                    <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full">
-                      {notificationcount}
-                    </span>
-                  ) : (
-                    ""
-                  )}
-                </button>
-
-                <div className="relative ml-4 flex text-white items-center gap-3 ">
+          >
+            <Header
+              setShowMenu={() =>
+                width > 1023 ? setShowMenu(true) : setShowMenu(!showMenu)
+              }
+            />
+          </aside>
+          <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] h-screen bg-background">
+            <div className="bg-gradient-to-t from-[#1a1c1e] to-[#212325]  border-gray-200">
+              <div className="flex items-center justify-between px-2 sm:px-4 py-2">
+                <div className="w-full flex items-center justify-between pr-5">
                   <button
-                    onClick={() => setShowUserMenu(!showUserMenu)}
-                    className="bg-blue-400 text-white rounded-full w-10 h-10 flex items-center justify-center"
+                    className="p-2 hover:bg-gray-100 rounded lg:hidden "
+                    onClick={() => setShowMenu(!showMenu)}
                   >
-                    {getInitials(authenticate?.full_name)}
+                    <AiOutlineMenu className="w-5 h-5" />
                   </button>
-                  {authenticate?.role}
 
-                  {/* {showUserMenu && (
+                  <h1 className="text-base sm:text-xl text-white font-semibold truncate capitalize">
+                    {location.pathname !== "/"
+                      ? temp?.replace("-", " ")
+                      : "Dashboard"}
+                  </h1>
+                  <div className=" flex items-end justify-end">
+                    <button
+                      onClick={() => setSidebarOpen(true)}
+                      className="relative flex items-center gap-2 bg-gradient-to-bl from-[#333333] to-[#666666] text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                    >
+                      <FaBell className="w-6 h-6" />
+                      {notificationcount > 0 ? (
+                        <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full">
+                          {notificationcount}
+                        </span>
+                      ) : (
+                        ""
+                      )}
+                    </button>
+
+                    <div className="relative ml-4 flex text-white items-center gap-3 ">
+                      <button
+                        onClick={() => setShowUserMenu(!showUserMenu)}
+                        className="bg-blue-400 text-white rounded-full w-10 h-10 flex items-center justify-center"
+                      >
+                        {getInitials(authenticate?.full_name)}
+                      </button>
+                      {authenticate?.role}
+
+                      {/* {showUserMenu && (
                     <div className="absolute right-0 mt-2 w-64 bg-[#4f4f4f] rounded-lg shadow-lg z-50">
                       <div className="flex items-center gap-2 px-4 py-3 border-b">
                         <div className="bg-blue-400 text-white rounded-full w-10 h-10 flex items-center justify-center font-bold">
@@ -160,27 +160,27 @@ transition duration-300 sm:w-[40%] md:w-[30%] lg:w-[25%] xl:w-[20%] 2xl:w-[15%] 
                       </ul>
                     </div>
                   )} */}
+                    </div>
+                  </div>
+
+                  <NotificationSidebar
+                    notificationsViewed={NotificationsViewed}
+                    notifications={notificationData}
+                    isOpen={isSidebarOpen}
+                    onClose={() => setSidebarOpen(false)}
+                  />
                 </div>
               </div>
-
-              <NotificationSidebar
-                notificationsViewed={NotificationsViewed}
-                notifications={notificationData}
-                isOpen={isSidebarOpen}
-                onClose={() => setSidebarOpen(false)}
-              />
             </div>
+            <Outlet />
           </div>
-        </div>
-        <Outlet />
-      </div>
-    
-      <Footer />
-      <UserProfile showUserMenu={showUserMenu} setShowMenu={setShowUserMenu} />
-  
 
-      <ChangePasswordModal isOpen={isOpen} onClose={closeModal} />
-    </Suspense>)
+          <Footer />
+          <UserProfile showUserMenu={showUserMenu} setShowMenu={setShowUserMenu} />
+
+
+          <ChangePasswordModal isOpen={isOpen} onClose={closeModal} />
+        </Suspense>)
   );
 };
 
