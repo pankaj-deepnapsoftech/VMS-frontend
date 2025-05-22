e10a61583351a00665f736cc60026dc1c60cc08b
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-05-22 12:03:05 +0530

    new Changes

--- src/pages/FirstDashboard.jsx
+++ src/pages/FirstDashboard.jsx
@@ -5,15 +5,16 @@ import { useNavigate } from "react-router-dom";
 import "./animation.css";
 import { ArrowDown, ArrowLeft, ArrowRight, ArrowUp } from "lucide-react";
 import { products } from "@/constants/static.data";
+import { getInitials } from "@/utils/profile";
 
 // Card component with gradient border
 const Card = ({ children, gradient, HandleClick }) => {
   return (
     <div
-      onClick={HandleClick}
-      className={`p-[1px] rounded-xl bg-gradient-to-br ${gradient} transform transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer max-w-sm w-full`}
+
+      className={`p-[1px]`}
     >
-      <div className="rounded-xl text-center bg-[#1c1c1e] p-6 min-h-[80px] backdrop-blur-md bg-opacity-80 transition-colors duration-300">
+      <div onClick={HandleClick} className={`rounded-xl bg-gradient-to-br ${gradient}/40 transform transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer max-w-sm w-full flex item-center justify-center h-16`}>
         {children}
       </div>
     </div>
@@ -22,15 +23,7 @@ const Card = ({ children, gradient, HandleClick }) => {
 
 
 // Function to get initials from full name 
-function getInitials(fullName) {
-  return (
-    fullName
-      ?.split(" ")
-      .filter(Boolean)
-      .map((item) => item[0].toUpperCase())
-      .join("") || "A"
-  );
-}
+
 
 const Dashboard = () => {
   const { authenticate, Logout, setGetDataFromSession } = useAuthContext();
@@ -99,143 +92,63 @@ const Dashboard = () => {
         <p className="mt-2 text-gray-400">Welcome to risk operations center</p>
       </div>
 
-
-      <div className="flex flex-col items-center gap-8 pt-10">
-        {/* Header */}
-        <div className="rounded-md bg-[#191925] p-4 text-white text-center  h-24 flex items-center justify-center">
-          Secure& Vulnerability & Risk Operations Centre (VROC)
+      <div className="flex gap-2 h-[90%] items-center -translate-y-16 ">
+        {/* right div */}
+        <div className="md:w-[30%] h- items-center  justify-center   text-5xl font-semibold  text-white ">
+          <span className="text-blue-400" >
+            Secure& 
+          </span>
+<br/> AI-Powered <br/> VROC
         </div>
 
-        {/* Second Row */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full max-w-5xl">
-          <Card
-            HandleClick={() => HandleClick(products[0].title)}
-            gradient={products[0].gradient}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[0].title}
-            </h3>
-          </Card>
+        {/* left div */}
+        <div className="flex flex-col items-center gap-8 pt-10">
 
-          <Card
-            HandleClick={() => HandleClick(products[1].title)}
-            gradient={products[1].gradient}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[1].title}
-            </h3>
-          </Card>
 
-          <Card
-            HandleClick={() => HandleClick(products[2].title)}
-            gradient={products[2].gradient}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[2].title}
-            </h3>
-          </Card>
 
-          <Card
-            gradient={products[3].gradient}
-            HandleClick={() => HandleClick(products[3].title)}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[3].title}
-            </h3>
-          </Card>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 w-full max-w-5xl">
+
+            {products.map((item, index) => {
+              if (index <= 4) {
+                return <Card key={index}
+                  HandleClick={() => HandleClick(item?.title)}
+                  gradient={item?.gradient}
+                >
+                  <h3 className="text-sm font-semibold text-gray-200 group-hover:text-blue-300 flex h-full w-full items-center justify-center">
+                    {item?.title}
+                  </h3>
+                </Card>
+              }
+            })}
+
+          </div>
+
+          {/* Middle Section with Arrows */}
+          <div className="rounded-md bg-[#191925] p-4 text-white text-center w-full max-w-5xl h-24 flex items-center justify-center text-2xl">
+            Virtual  Risk Operations Centre (VROC)
+          </div>
+
+          {/* Bottom Row */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 w-full max-w-5xl">
+            {products.map((item, index) => {
+              if (index > 4) {
+                return <Card key={index}
+                  HandleClick={() => HandleClick(item?.title)}
+                  gradient={item?.gradient}
+                >
+                  <h3 className="text-sm font-semibold text-gray-200 group-hover:text-blue-300 flex h-full w-full items-center justify-center">
+                    {item?.title}
+                  </h3>
+                </Card>
+              }
+            })}
+          </div>
+
+
         </div>
-
-        {/* Middle Section with Arrows */}
-        <div className="relative w-full max-w-5xl flex justify-center">
-          <div className="hidden md:flex absolute top-0 left-1/2 -translate-x-1/2 w-[60%] h-16 border-b-2 border-l-2 border-r-2 border-blue-700"></div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-5xl z-10">
-            <div className="md:col-start-1 md:col-end-2 flex justify-center">
-              <div
-                className={`p-[1px] rounded-md bg-gradient-to-br ${products[3].gradient} w-full max-w-xs transition-transform transform hover:scale-105`}
-              >
-                <div className="rounded-md bg-[#1c1c1e] p-4 text-white text-center h-24 flex items-center justify-center">
-                  AI-VA
-                </div>
-              </div>
-            </div>
-            <div className="md:col-start-2 md:col-end-3 flex justify-center">
-              <div
-                className={`p-[1px] rounded-md bg-gradient-to-br ${products[2].gradient} w-full max-w-xs transition-transform transform hover:scale-105`}
-              >
-                <div className="rounded-md bg-[#1c1c1e] p-4 text-white text-center h-24 flex items-center justify-center">
-                  Vulnerability Intelligence
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Arrows for mobile */}
-          <div className="flex md:hidden flex-col items-center gap-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <ArrowUp className="text-blue-700" />
-            <ArrowDown className="text-blue-700" />
-          </div>
-
-          {/* Arrows for desktop */}
-          <div className="hidden md:block absolute top-0 left-[25%] -translate-x-1/2 mt-16">
-            <ArrowRight className="text-blue-700" />
-          </div>
-          <div className="hidden md:block absolute top-0 right-[25%] translate-x-1/2 mt-16">
-            <ArrowLeft className="text-blue-700" />
-          </div>
-        </div>
-
-        {/* Bottom Row */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full max-w-5xl">
-          <Card
-            gradient={products[4].gradient}
-            HandleClick={() => HandleClick(products[4].title)}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[4].title}
-            </h3>
-          </Card>
-
-          <Card
-            gradient={products[5].gradient}
-            HandleClick={() => HandleClick(products[5].title)}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[5].title}
-            </h3>
-          </Card>
-
-          <Card
-            gradient={products[6].gradient}
-            HandleClick={() => HandleClick(products[6].title)}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[6].title}
-            </h3>
-          </Card>
-
-          <Card
-            gradient={products[7].gradient}
-            HandleClick={() => HandleClick(products[7].title)}
-          >
-            <h3 className="text-lg font-semibold text-gray-200 group-hover:text-blue-300">
-              {products[7].title}
-            </h3>
-          </Card>
-        </div>
-
-        {/* Footer */}
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-5xl justify-end">
-          <div className="sm:col-start-2 sm:col-end-3">
-            <div className="bg-emerald-500 text-white p-4 flex items-center justify-center text-center rounded-md h-12 mb-4">
-              In House
-            </div>
-            <div className="bg-blue-700 text-white p-4 flex items-center justify-center text-center rounded-md h-12">
-              Integrations
-            </div>
-          </div>
-        </div> */}
       </div>
+
+
     </div>
   );
 };
