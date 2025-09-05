f83e9cdc6374f64429253636c972f79f63abc455
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-09-05 16:03:27 +0530

    Some change

--- src/Validation/SchedulingAssessmentValidation.jsx
+++ src/Validation/SchedulingAssessmentValidation.jsx
@@ -1,4 +1,4 @@
-import { boolean, object, string, mixed } from 'yup';
+import { boolean,date, object, string, mixed } from 'yup';
 
 export const SchedulingAssessmentValidation = object({
     Type_Of_Assesment: string().required('Type Of Assesment is Required'),
@@ -7,6 +7,15 @@ export const SchedulingAssessmentValidation = object({
     // Select_Tester: string().required(' Select Tester is Required'),
     // code_Upload: mixed().required('A file is required'),
     MFA_Enabled: boolean().required('MFA Enabled is Required'),
-    Start_Date: boolean().required('Preferred Task Start Date Required'),
-    End_Date: boolean().required('Preferred Task End Date Required'),
+    Start_Date: date()
+    .required('Preferred Task Start Date Required')
+    .min(new Date(), 'Start date cannot be in the past'),
+
+  End_Date: date()
+    .required('Preferred Task End Date Required')
+    .when('Start_Date', (startDate, schema) =>
+      startDate
+        ? schema.min(startDate, 'End date cannot be before start date')
+        : schema
+    ),
 });
