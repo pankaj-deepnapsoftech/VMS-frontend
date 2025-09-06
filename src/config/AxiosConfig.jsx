74bafeacf10b62441ca0b54da8706eabaed02728
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-09-06 15:25:19 +0530

    some change

--- src/config/AxiosConfig.jsx
+++ src/config/AxiosConfig.jsx
@@ -3,20 +3,23 @@ import axios from "axios";
 import Cookies from 'js-cookie';
 
 
-const token = Cookies.get('AT'); 
+const token = Cookies.get('AT');
 
 const NewDecryptionToken = (tk) => {
+    if (!tk) {
+        return ""
+    }
     const data = decrypt(tk)
-    const newToken = encrypt({...data,frontend:true});
+    const newToken = encrypt({ ...data, frontend: true });
     return newToken
 }
 
-export const AxiosHandler  =  axios.create({
-    
-    baseURL:import.meta.env.VITE_BACKEND_BASE_URL,
-    withCredentials:true,
-    headers:{
-        Authorization:`Bearer ${NewDecryptionToken(token)}`
+export const AxiosHandler = axios.create({
+
+    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+    withCredentials: true,
+    headers: {
+        Authorization: `Bearer ${NewDecryptionToken(token)}`
     }
 
 })
