8ecd931f92499ef8f18ae2c6597438e8bfe61dac
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-09-11 11:31:05 +0530

    Some Change

--- src/Validation/RoleValidations.jsx
+++ src/Validation/RoleValidations.jsx
@@ -1,18 +1,15 @@
 import * as Yup from 'yup';
 
-  export const RoleSchema = Yup.object({
+export const RoleSchema = Yup.object({
     role: Yup.string()
         .trim()
         .required('Role name is required')
         .min(3, 'Role name must be at least 3 characters')
         .max(50, 'Role name cannot exceed 50 characters'),
-
-    allowed_path: Yup.array()
-        .min(1, 'Please select at least one path')
-        .of(
-            Yup.object().shape({
-                name: Yup.string().required('Path name is required'),
-                
-            })
-        )
+    description: Yup.string()
+        .trim()
+        .required('Description is required')
+        .min(10, 'Description must be at least 10 characters')
+        .max(200, 'Description cannot exceed 200 characters'),
+    
 });
