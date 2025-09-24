7be09f2072a9b668531542943006eaea7f9cf71c
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-09-24 11:19:47 +0530

    sidebar some change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,9 +28,9 @@ function Header({ setShowMenu, showSidebar }) {
   };
 
   return (
-    <div className="flex flex-col text-white h-[100%] hide-scrollbar bg-[#1f2937] overflow-y-auto transition-all duration-500 ease-in-out">
+    <div className="flex flex-col text-white h-[100%] hide-scrollbar bg-[#1f2937] overflow-y-auto transition-all duration-500 ease-in-out relative">
       <hr className="border-gray-100 mx-8" />
-      <nav className="flex-1 mx-2 py-5 space-y-1">
+      <nav className="flex-1 mx-2 py-5 space-y-1 hide-scrollbar overflow-scroll pb-72">
         {products.map((item, index) => (
           <React.Fragment key={index}>
             {/* here is show title only */}
@@ -68,9 +68,9 @@ function Header({ setShowMenu, showSidebar }) {
           </React.Fragment>
         ))}
       </nav>
-      <hr className="border-gray-100 mx-8" />
-      <div className={`h-full p-2 flex justify-start items-end ${showSidebar ? "" : "block lg:hidden"}`}>
-        <div className="relative p-5 flex">
+      <hr className="border-gray-100 mx-8 " />
+      <div className={`h-20 fixed -bottom-16 p-2 flex w-full  justify-start items-end bg-[#1f2937] ${showSidebar ? "" : "block lg:hidden"}`}>
+        <div className="p-5 flex bg-[#1f2937]">
           <button
             onClick={handleLogout}
             className="flex w-full px-10 mb-52 md:mb-24 items-center text-indigo-600 hover:scale-95 transition bg-[#3533cc] rounded-lg justify-center"
