c50a989a382be39fd52ca594a369e8fd370aacee
deepnapsoft-pankaj <itsybizz@gamil.com>
2025-10-27 11:36:51 +0530

    some changes

--- src/context/MainReportContext/MainReportContext.jsx
+++ src/context/MainReportContext/MainReportContext.jsx
@@ -3,78 +3,90 @@ import { createContext, useState } from "react";
 import { useAuthContext } from "..";
 import toast from "react-hot-toast";
 
-
 export const MainReportContext = createContext({
-    GetAllReports: () => { },
-    uploadReports: () => { },
-    DeleteReport: () => { },
-    UpdateReport: () => { },
-    reportsData: []
+  GetAllReports: () => {},
+  uploadReports: () => {},
+  DeleteReport: () => {},
+  UpdateReport: () => {},
+  reportsData: [],
+  downloadData: [],
 });
-
 
 // eslint-disable-next-line react/prop-types
 const MainReportContextProvider = ({ children }) => {
+  const { tenant } = useAuthContext();
 
-    const { tenant } = useAuthContext();
+  const [reportsData, setReportsData] = useState([]);
 
-    const [reportsData, setReportsData] = useState([]);
+  const [downloadData, setDownloadData] = useState([]);
 
-    const GetAllReports = async (tenant) => {
-        try {
-            const res = await AxiosHandler(`/report/get-report?tenat=${tenant ? tenant : ""}`);
-            setReportsData(res.data?.data)
-        } catch (error) {
-            console.log(error);
-        }
+  const GetAllReports = async (tenant) => {
+    try {
+      const res = await AxiosHandler(
+        `/report/get-report?tenat=${tenant ? tenant : ""}`
+      );
+      setReportsData(res.data?.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-    const uploadReports = async (data) => {
-        try {
-            const res = await AxiosHandler.post("/report/detailed-report", data);
-            GetAllReports(tenant)
-            toast.success(res.data.message || "Report Upload Successfully")
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-
-
-
-    const DeleteReport = async (id) => {
-        if (!window.confirm("Are you sure you want to delete ")) return;
-        try {
-            const res = await AxiosHandler.delete(`/report/delete-report/${id}`);
-            toast.success(res.data.message || "Report delete Successfully");
-            GetAllReports(tenant)
-        } catch (error) {
-            console.log(error)
-        }
-
+  const uploadReports = async (data) => {
+    try {
+      const res = await AxiosHandler.post("/report/detailed-report", data);
+      GetAllReports(tenant);
+      toast.success(res.data.message || "Report Upload Successfully");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const UpdateReport = async (id,data) => {
-        try {
-            const res = await AxiosHandler.put(`/report/update-report/${id}`,data);
-            toast.success(res.data.message || "Report delete Successfully");
-            GetAllReports(tenant)
-        } catch (error) {
-            console.log(error)
-        }
-
+  const DeleteReport = async (id) => {
+    if (!window.confirm("Are you sure you want to delete ")) return;
+    try {
+      const res = await AxiosHandler.delete(`/report/delete-report/${id}`);
+      toast.success(res.data.message || "Report delete Successfully");
+      GetAllReports(tenant);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <MainReportContext.Provider value={{ GetAllReports, reportsData, uploadReports, DeleteReport,UpdateReport }}>
-            {children}
-        </MainReportContext.Provider>
-    )
+  const UpdateReport = async (id, data) => {
+    try {
+      const res = await AxiosHandler.put(`/report/update-report/${id}`, data);
+      toast.success(res.data.message || "Report delete Successfully");
+      GetAllReports(tenant);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const DownloadReport = async () => {
+    try {
+      const res = await AxiosHandler.get(
+        `/report/download-all-vulnerabilities`
+      );
+      setDownloadData(res.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <MainReportContext.Provider
+      value={{
+        GetAllReports,
+        reportsData,
+        uploadReports,
+        DeleteReport,
+        UpdateReport,
+        DownloadReport,
+      }}
+    >
+      {children}
+    </MainReportContext.Provider>
+  );
 };
 
-export default MainReportContextProvider
-
-
-
-
+export default MainReportContextProvider;
