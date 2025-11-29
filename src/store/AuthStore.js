cb619a6519226e2df680e64aa1d409a50d7e2f89
deepnapsoft-pankaj <itsybizz@gamil.com>
2025-11-29 11:50:57 +0530

    some changes

--- src/store/AuthStore.js
+++ src/store/AuthStore.js
@@ -1,40 +1,38 @@
 import Cookies from "js-cookie";
 import { create } from "zustand";
 
-
 export const useAuthStore = create((set) => ({
-    token:Cookies.get("AT") || "",
-    authenticate: sessionStorage.getItem("auth") ? JSON.parse(sessionStorage.getItem("auth")) : null,
-    selectedYears:new Date().getFullYear(),
-    tenant:sessionStorage.getItem('tenant') ? JSON.parse(sessionStorage.getItem('tenant'))?.value : null,
+  token: Cookies.get("AT") || "",
+  authenticate: sessionStorage.getItem("auth")
+    ? JSON.parse(sessionStorage.getItem("auth"))
+    : null,
+  selectedYears: new Date().getFullYear(),
+  tenant: sessionStorage.getItem("tenant")
+    ? JSON.parse(sessionStorage.getItem("tenant"))?.value
+    : null,
 
-    //========== UserProfile in MainLayout ========================
-    showUserMenu: false,
-    setShowUserMenu: (value) => set(() => ({ showUserMenu: value })),
+  //========== UserProfile in MainLayout ========================
+  showUserMenu: false,
+  setShowUserMenu: (value) => set(() => ({ showUserMenu: value })),
 
+  //==============updateProfileModal in MainLayout ======================
+  updateProfileModal: false,
+  setUpdateProfileModal: (value) => set(() => ({ updateProfileModal: value })),
 
-    //==============updateProfileModal in MainLayout ======================
-    updateProfileModal: false,
-    setUpdateProfileModal: (value) => set(()=>({updateProfileModal: value})),
+  //============ SideBar ====================================
+  OpenSideBar: false,
+  setOpenSideBar: (value) => set(() => ({ OpenSideBar: value })),
 
-
-    //============ SideBar ====================================
-    OpenSideBar: false,
-    setOpenSideBar: (value) => set(()=>({OpenSideBar: value})),
-
-
-    //======== VROC MODULE SELECTION =======================
-    getDataFromSession: sessionStorage.getItem("VROC") || "",
-    setGetDataFromSession: (value) => {
+  //======== VROC MODULE SELECTION =======================
+  getDataFromSession: sessionStorage.getItem("VROC") || "",
+  setGetDataFromSession: (value) => {
     sessionStorage.setItem("VROC", value);
     set({ getDataFromSession: value });
-    },
+  },
 
-
-    // ===================== all functions =====================
-    setToken:(token) => set(()=>({token})),
-    setSelectedYear:(selectedYears) => set(() => ({selectedYears})),
-    setAuthenticate:(authenticate) => set(()=>({authenticate})),
-    setTenant:(tenant) => set(()=>({tenant}))
-
+  // ===================== all functions =====================
+  setToken: (token) => set(() => ({ token })),
+  setSelectedYear: (selectedYears) => set(() => ({ selectedYears })),
+  setAuthenticate: (authenticate) => set(() => ({ authenticate })),
+  setTenant: (tenant) => set(() => ({ tenant })),
 }));
