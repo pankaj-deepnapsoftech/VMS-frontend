b90549b4bcd038b67280d5838869538793333a80
pankaj <deepak@gmal.com>
2025-12-03 17:34:15 +0530

    iconsss

--- src/constants/Icons.jsx
+++ src/constants/Icons.jsx
@@ -1,132 +1,160 @@
-export {
-  IoMdCloseCircleOutline,
-  IoIosArrowDown,
-  IoIosLogOut,
-  IoIosInformationCircleOutline,
-  IoIosClose,
-} from "react-icons/io";
-
-export { RxCross2 } from "react-icons/rx";
+// Replaced with lucide-react icons while keeping SAME export names
+// NOTE: Lucide does not have 1:1 icons for everything, closest alternatives used
 
 export {
-  MdClose,
-  MdInventory2,
-  MdOutlineMiscellaneousServices,
-  MdOutlinePendingActions,
-  MdScheduleSend,
-  MdSettingsApplications,
-  MdWifiTetheringErrorRounded,
-  MdOutlineShield,
-  MdDelete,
-  MdModeEditOutline,
-  MdDeleteForever,
-  MdError,
-  MdOutlineNotificationsActive,
-} from "react-icons/md";
+  CircleX as IoMdCloseCircleOutline,
+  ChevronDown as IoIosArrowDown,
+  LogOut as IoIosLogOut,
+  Info as IoIosInformationCircleOutline,
+  X as IoIosClose,
+} from "lucide-react";
+
+export { X as RxCross2 } from "lucide-react";
+
 export {
-  IoCloudUploadOutline,
-  IoClose,
-  IoShield,
-  IoShieldHalf,
-  IoSettingsOutline,
-  IoCloseSharp,
-  IoSearch,
-  IoWarningOutline,
-  IoMailSharp,
-  IoHomeOutline,
-} from "react-icons/io5";
+  X as MdClose,
+  Boxes as MdInventory2,
+  Wrench as MdOutlineMiscellaneousServices,
+  Clock as MdOutlinePendingActions,
+  Send as MdScheduleSend,
+  Settings as MdSettingsApplications,
+  WifiOff as MdWifiTetheringErrorRounded,
+  Shield as MdOutlineShield,
+  Trash2 as MdDelete,
+  Pencil as MdModeEditOutline,
+  Trash2 as MdDeleteForever,
+  AlertCircle as MdError,
+  BellRing as MdOutlineNotificationsActive,
+} from "lucide-react";
+
 export {
-  FaTimesCircle,
-  FaBell,
-  FaTimes,
-  FaEye,
-  FaEyeSlash,
-  FaRegBookmark,
-  FaBug,
-  FaUsers,
-  FaCheckCircle,
-  FaUserCircle,
-  FaLock,
-  FaChevronUp,
-  FaSms,
-  FaChevronRight,
-  FaFacebookF,
-  FaTwitter,
-  FaYoutube,
-  FaProjectDiagram,
-  FaSearch,
-  FaTools,
-  FaRegTrashAlt,
-  FaEnvelope,
-  FaPhone,
-  FaUser,
-  FaEdit,
-  FaArrowUp,
-  FaExternalLinkAlt,
-  FaRegQuestionCircle,
-  FaDownload,
-  FaChartBar,
-  FaUserCheck,
-  FaRegUserCircle,
-  FaClock,
-  FaPlug,
-  FaTrash,
-} from "react-icons/fa";
+  UploadCloud as IoCloudUploadOutline,
+  X as IoClose,
+  Shield as IoShield,
+  ShieldHalf as IoShieldHalf,
+  Settings as IoSettingsOutline,
+  X as IoCloseSharp,
+  Search as IoSearch,
+  AlertTriangle as IoWarningOutline,
+  Mail as IoMailSharp,
+  Home as IoHomeOutline,
+} from "lucide-react";
+
 export {
-  BiErrorAlt,
-  BiSolidDashboard,
-  BiSearch,
-  BiEditAlt,
-  BiPlus,
-  BiDetail,
-  BiUpload,
-} from "react-icons/bi";
-export { AiOutlineException } from "react-icons/ai";
+  CircleX as FaTimesCircle,
+  Bell as FaBell,
+  X as FaTimes,
+  Eye as FaEye,
+  EyeOff as FaEyeSlash,
+  Bookmark as FaRegBookmark,
+  Bug as FaBug,
+  Users as FaUsers,
+  CheckCircle as FaCheckCircle,
+  UserCircle as FaUserCircle,
+  Lock as FaLock,
+  ChevronUp as FaChevronUp,
+  MessageCircle as FaSms,
+  ChevronRight as FaChevronRight,
+  Facebook as FaFacebookF,
+  Twitter as FaTwitter,
+  Youtube as FaYoutube,
+  Projector as FaProjectDiagram,
+  Search as FaSearch,
+  Wrench as FaTools,
+  Trash2 as FaRegTrashAlt,
+  Mail as FaEnvelope,
+  Phone as FaPhone,
+  User as FaUser,
+  Pencil as FaEdit,
+  ArrowUp as FaArrowUp,
+  ExternalLink as FaExternalLinkAlt,
+  CircleHelp as FaRegQuestionCircle,
+  Download as FaDownload,
+  BarChart2 as FaChartBar,
+  UserCheck as FaUserCheck,
+  UserCircle as FaRegUserCircle,
+  Clock as FaClock,
+  Plug as FaPlug,
+  Trash as FaTrash,
+} from "lucide-react";
+
 export {
-  GrVulnerability,
-  GrStatusGood,
-  GrEdit,
-  GrShieldSecurity,
-} from "react-icons/gr";
-export { SiWikimediafoundation } from "react-icons/si";
-export { TbReport, TbLockPassword, TbStatusChange } from "react-icons/tb";
-export { SlCalender } from "react-icons/sl";
-export { CiViewTable } from "react-icons/ci";
+  AlertTriangle as BiErrorAlt,
+  LayoutDashboard as BiSolidDashboard,
+  Search as BiSearch,
+  Pencil as BiEditAlt,
+  Plus as BiPlus,
+  List as BiDetail,
+  Upload as BiUpload,
+} from "lucide-react";
+
+export { AlertCircle as AiOutlineException } from "lucide-react";
+
 export {
-  GiCyberEye,
-  GiProgression,
-  GiPerspectiveDiceSixFacesFour,
-  GiTwirlCenter,
-} from "react-icons/gi";
-export { PiHardHatFill } from "react-icons/pi";
-export { RiHome3Fill, RiDeleteBinFill, RiEdit2Line } from "react-icons/ri";
+  ShieldCheck as GrVulnerability,
+  CheckCircle as GrStatusGood,
+  Pencil as GrEdit,
+  Shield as GrShieldSecurity,
+} from "lucide-react";
+
+export { CirclePower  as SiWikimediafoundation } from "lucide-react";
+
 export {
-  LuAirVent,
-  LuCassetteTape,
-  LuChartNoAxesCombined,
-  LuComponent,
-  LuFileUp,
-} from "react-icons/lu";
+  FileClock as TbReport,
+  KeyRound as TbLockPassword,
+  RefreshCw as TbStatusChange,
+} from "lucide-react";
+
+export { Calendar as SlCalender } from "lucide-react";
+
+export { Table as CiViewTable } from "lucide-react";
+
 export {
-  BsBoxes,
-  BsPersonCheckFill,
-  BsThreeDotsVertical,
-} from "react-icons/bs";
+  Eye as GiCyberEye,
+  TrendingUp as GiProgression,
+  Dice6 as GiPerspectiveDiceSixFacesFour,
+  Orbit as GiTwirlCenter,
+} from "lucide-react";
+
+export { HardHat as PiHardHatFill } from "lucide-react";
+
 export {
-  FiLink2,
-  FiMenu,
-  FiTrendingUp,
-  FiTrendingDown,
-  FiAlertCircle,
-  FiAlertTriangle,
-  FiMinusCircle,
-  FiEdit2,
-  FiTrash2,
-} from "react-icons/fi";
+  Home as RiHome3Fill,
+  Trash as RiDeleteBinFill,
+  Pencil as RiEdit2Line,
+} from "lucide-react";
+
 export {
-  FaArrowPointer,
-  FaMessage,
-  FaPerson,
-  FaFireFlameCurved,
-} from "react-icons/fa6";
-export { AiOutlineMenu } from "react-icons/ai";
+  Wind as LuAirVent,
+  Videotape  as LuCassetteTape,
+  BarChart as LuChartNoAxesCombined,
+  Component as LuComponent,
+  Upload as LuFileUp,
+} from "lucide-react";
+
+export {
+  Boxes as BsBoxes,
+  UserCheck as BsPersonCheckFill,
+  MoreVertical as BsThreeDotsVertical,
+} from "lucide-react";
+
+export {
+  Link as FiLink2,
+  Menu as FiMenu,
+  TrendingUp as FiTrendingUp,
+  TrendingDown as FiTrendingDown,
+  AlertCircle as FiAlertCircle,
+  AlertTriangle as FiAlertTriangle,
+  MinusCircle as FiMinusCircle,
+  Pencil as FiEdit2,
+  Trash2 as FiTrash2,
+} from "lucide-react";
+
+export {
+  MousePointer as FaArrowPointer,
+  MessageSquare as FaMessage,
+  User as FaPerson,
+  Flame as FaFireFlameCurved,
+} from "lucide-react";
+
+export { Menu as AiOutlineMenu } from "lucide-react";
